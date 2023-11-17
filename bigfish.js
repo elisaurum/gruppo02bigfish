7c9986f2f6a9ad7b595c879927ff0644b81aeb0b
elisaurum <145792002+elisaurum@users.noreply.github.com>
2023-11-17 19:21:14 +0100

    aggiunta linee e stile

--- bigfish.js
+++ bigfish.js
@@ -3,16 +3,26 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 const datasetprova = await d3.csv("datasetprova.csv")
 
-const height = 10000;
-const marginLeft = 50;
-const marginTop = 50;
+const height = 15100;
+const marginLeft = 200;
+const marginRight = 50;
+const marginTop = 150;
 const marginBottom = 50;
 const width = window.innerWidth - marginLeft;
 
 // Creazione di un elemento SVG
 const svg = d3.create("svg")
-    .attr("height", height)
-    .attr("width", width);
+.attr("height", height)
+.attr("width", width);
+
+// Nuovo formato del minutaggio
+function formatTime(minutes) {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    const formattedHours = hours < 10 ? `0${hours}` : hours;
+    const formattedMinutes = remainingMinutes < 10 ? `0${remainingMinutes}` : remainingMinutes;
+    return `${formattedHours}:${formattedMinutes}`;
+}
 
 // Creazione di una scala lineare da 0 a 120
 const yScale = d3.scaleLinear()
@@ -21,8 +31,17 @@ const yScale = d3.scaleLinear()
 
 // Creazione di una scala lineare basata sulla colonna "START" del dataset
 const sScale = d3.scaleLinear()
-.range([marginTop, height - marginBottom*38.4]) // Per far sì che rientri nella scala
+.range([marginTop, height - marginBottom*58.55]) // Per far sì che rientri nella scala
 .domain(d3.extent(datasetprova, d => d["START"]));
+
+// Estrazione dei personaggi dalle colonne dalla 3 in poi
+const personaggi = Object.keys(datasetprova[0]).slice(3)
+
+// Creazione di una scala scaleBand basata sui personaggi
+const xScale = d3.scaleBand()
+.domain(personaggi)
+.range([marginLeft, width - marginRight])
+.padding(7)
 
 
 
@@ -35,13 +54,31 @@ const gruppi = svg.selectAll("g")
 svg.append("g")
 .attr("transform", "translate(" + marginLeft + " , 0)")
 .call(d3.axisLeft(yScale)
-.tickValues([yScale.domain()[1]]));
+.tickFormat(d => d === 120 ? "02:00" : d)
+.tickValues([yScale.domain()[1]]))
+.attr("class", "axis-label");
 
 // Aggiungo l'asse a sinistra con sScale con i tick corrispondenti ai valori della scala (e quindi all'inizio della scena)
 svg.append("g")
 .attr("transform", "translate(" + marginLeft + " , 0)")
 .call(d3.axisLeft(sScale)
-.tickValues(datasetprova.map(d => d["START"])));
+.tickFormat(formatTime)
+.tickValues(datasetprova.map(d => d["START"])))
+.selectAll(".tick text")
+.attr("class", "axis-label");
+
+// Aggiungo l'asse x e aggiusto le etichette
+svg.append("g")
+.attr("transform", "translate(0, " + marginTop + ")")
+.call(d3.axisBottom(xScale).tickSize(0))
+.call((g) => g.select (".domain").remove())
+.selectAll("text")
+.attr("text-anchor", "start")
+.attr("class", "axis-label")
+.attr("dx", "10 em") // Regola la posizione orizzontale
+.attr("dy", "0 em") // Regola la posizione verticale
+.attr("transform", "rotate(-90)")
+.text(d => d.replace(/_/g, ' ')) // Per togliere l'underscore
 
 
 
@@ -56,7 +93,7 @@ gruppi.append("rect")
 .attr("height", (d, i) => {
 return sScale(heightData[i]); // Calcolo dell'altezza dei rettangoli (unico modo per non avere errore nella parte dopo)
 })
-.attr("width", width - marginLeft)
+.attr("width", width - marginLeft - marginRight)
 .attr("x", marginLeft)
 .attr("y", d => sScale(d["START"]))
 .attr("fill", (d, i) => colors[i]);
@@ -71,14 +108,52 @@ const brightness = (rgb.r * 0.299 + rgb.g * 0.587 + rgb.b * 0.114) / 255;
 return brightness > 0.5; // Se la luminosità è superiore a 0.5, il colore è considerato chiaro
 }
 
-// Aggiungta del testo al centro di ciascun rettangolo
+// Aggiunta del testo al centro di ciascun rettangolo
 gruppi.append("text")
 .text((d, i) => d["PLACE"]) // Prende il valore dalla colonna PLACE
-.attr("x", marginLeft + (width - marginLeft) / 2)
-.attr("y", (d, i) => sScale(d["START"]) + sScale(heightData[i])/2)
+.attr("x", marginLeft + (width - marginLeft - marginRight) / 2)
+.attr("y", (d, i) => sScale(d["START"]) + sScale(heightData[i])/2.5)
 .attr("dy", "-0.7em")
 .attr("text-anchor", "middle")
 .attr("fill", d => isColorLight(d["COLOR"]) ? "black" : "white") // Esegue la funzione isColorLight riferendosi al valore HEX della colonna COLOR e determina se rendere il testo bianco o nero
 
-// Restituisci il nodo SVG
+// Mappatura dei colori per personaggio
+const colorMap = {
+    STREGA: "grey",
+    AMOS_CALLOWAY: "purple",
+    JENNY: "chartreuse",
+    DON_PRICE: "cornflowerblue",
+    SANDRA: "yellow",
+    EDWARD: "red",
+    KARL: "brown",
+    NORTHER_WINSLOW: "darkgreen",
+    JING_E_PING: "blue",
+    WILL: "orangered",
+    JOSEPHINE: "magenta"
+};
+
+// Aggiunta delle linee verticali per indicare la presenza di un personaggio
+datasetprova.columns.slice(3).forEach((personaggio) => {
+const isPresent = datasetprova.map(d => d[personaggio] === "P");
+const lineData = datasetprova.map((d, i) => ({
+    isPresent: isPresent[i],
+    start: d["START"],
+    nextStart: i < datasetprova.length - 1 ? datasetprova[i + 1]["START"] : height
+}));
+
+  
+svg.selectAll("line" + personaggio)
+    .data(lineData.filter(d => d.isPresent))
+    .enter()
+    .append("line")
+    .attr("x1", xScale(personaggio) + xScale.bandwidth() / 2)
+    .attr("x2", xScale(personaggio) + xScale.bandwidth() / 2)
+    .attr("y1", d => sScale(d.start))
+    .attr("y2", d => Math.min(sScale(d.nextStart), yScale.range()[1])) // Per farlo finire quando finisce l'asse y
+    .attr("stroke", colorMap[personaggio])
+    .attr("stroke-width", 12)
+
+});
+
+// Return del nodo SVG
 document.body.appendChild(svg.node());
